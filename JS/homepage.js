93d422304d831f05db93c6c469f83427ba2fa175
MEGHA2507 <megha.banerjee07@gmail.com>
2020-09-12 23:11:21 +0530

    homapage and animations added

---
+++ JS/homepage.js
@@ -0,0 +1,13 @@
+// Card hover Animation
+// const cardTile = document.querySelector('.card');
+
+// cardTile.addEventListener('mouseenter', openTile);
+// cardTile.addEventListener('mouseleave', openTile);
+
+// function openTile() {
+//     if (cardTile.classList.contains('animate__animated', 'animate__pulse')) {
+//         cardTile.classList.remove('animate__animated', 'animate__pulse');
+//     } else {
+//         cardTile.classList.add('animate__animated', 'animate__pulse');
+//     }
+// }
